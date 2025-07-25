237540a86983db791d54e65ed9782bd70b073e84
David Thomann <david.joshua.thomann@gmail.com>
2025-07-25 19:19:16 +0200

    Migrated Undo/Redo Functionality into MenuBar

--- app/src/main/java/pizzapal/ui/editor/EditorView.java
+++ app/src/main/java/pizzapal/ui/editor/EditorView.java
@@ -7,6 +7,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.ToolBar;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import pizzapal.model.storage.Storage;
 import pizzapal.model.storage.StorageController;
@@ -21,17 +22,33 @@ public class EditorView extends BorderPane {
         StorageView storageView = new StorageView(storageController);
 
         MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("Datei");
-        MenuItem newItem = new MenuItem("Neu");
-        MenuItem openItem = new MenuItem("Öffnen...");
-        MenuItem exitItem = new MenuItem("Beenden");
+
+        Menu fileMenu = new Menu("File");
+        MenuItem newItem = new MenuItem("New");
+        MenuItem openItem = new MenuItem("Open");
+        MenuItem exitItem = new MenuItem("Exit");
         fileMenu.getItems().addAll(newItem, openItem, new SeparatorMenuItem(), exitItem);
         menuBar.getMenus().addAll(fileMenu);
+
+        Menu editMenu = new Menu("Edit");
+        MenuItem undoItem = new MenuItem("Undo");
+        undoItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Z"));
+        undoItem.setOnAction(e -> {
+            storageController.undo();
+        });
+        MenuItem redoItem = new MenuItem("Redo");
+        redoItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Y"));
+        redoItem.setOnAction(e -> {
+            storageController.redo();
+        });
+        editMenu.getItems().addAll(undoItem, redoItem);
+        menuBar.getMenus().addAll(editMenu);
 
         ToolBar toolbar = new ToolBar();
         Button selectButton = new Button("Auswählen");
         Button drawButton = new Button("Zeichnen");
         Button eraseButton = new Button("Löschen");
+
         toolbar.setOrientation(Orientation.VERTICAL);
         toolbar.getItems().addAll(selectButton, drawButton, eraseButton);
 
